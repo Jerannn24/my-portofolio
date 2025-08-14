b470a560d3616b40693999048fe27db18fc550b4
Jerannn24 <juniornatra72@gmail.com>
2025-08-14 22:51:42 +0700

    update globals.css, layout.js and page.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
