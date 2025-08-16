50bfdb05c20495d32517df4ecbbf31eb6d38c7c8
Jerannn24 <juniornatra72@gmail.com>
2025-08-16 20:14:49 +0700

    update globals.css and Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,40 +7,47 @@ const Portfolio = () => {
     return (
         <div style={{ backgroundColor: "var(--bg)", minHeight: "100vh" }}>
             <Navbar />
-            <div className='container py-5' id='home'>
+            <div className='container py-5' id='about'>
                 <div className='row align-items-center'>
                     <div className='col-md-6 text-center'>
                         <img
                             src='./myself.png'
                             alt='Myself'
-                            className='img-fluid'
-                            style={{ maxWidth: '500px', borderRadius: '10px' }}
+                            className='img-fluid myself'
+                            style={{ maxWidth: '500px', borderRadius: '50px' }}
                         />
                     </div>
                     <div className='col-md-6 text-white'>
-                        <h1 className='fw-bold'>
-                            <Typewriter
-                                words={["I'm Junior Natra Situmorang"]}
-                                loop={1}             // atau false, supaya cuma sekali
-                                cursor={false}
-                                typeSpeed={70}
-                                deleteSpeed={0}
-                            />
-                        </h1>
-                        <p>
-                            <Typewriter
-                                words={["I was interested at Software Engineering"]}
-                                loop={true}
-                                cursor
-                                cursorStyle="|"
-                                typeSpeed={70}
-                                deleteSpeed={100}
-                                delaySpeed={1500} />
-                        </p>
+                        <div className='desc-writer'>
+                            <h1 className='fw-bold'>
+                                <Typewriter
+                                    words={["I'm Junior Natra Situmorang"]}
+                                    loop={1}             // atau false, supaya cuma sekali
+                                    cursor={false}
+                                    typeSpeed={70}
+                                    deleteSpeed={0}
+                                />
+                            </h1>
+                            <h5>
+                                <Typewriter
+                                    words={["I’m passionate about Software Engineering and excited to explore the world of AI Engineering 🤖. I love building things that solve real problems, experimenting with new ideas, and continuously learning along the way.", "Always open to collaboration, new opportunities, or just a chat — feel free to reach me anytime! ✉️"]}
+                                    loop={true}
+                                    cursor
+                                    cursorStyle="|"
+                                    typeSpeed={20}
+                                    deleteSpeed={50}
+                                    delaySpeed={1500} />
+                            </h5>
+                        </div>
+                        <a href='https://www.linkedin.com/in/junior-natra-929b42320/'>
+                            <button className='contactMe'>
+                                Contact Me
+                            </button>
+                        </a>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
