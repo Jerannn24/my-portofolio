d237d13061ce027c864a7b0acf247740d1329856
Jerannn24 <juniornatra72@gmail.com>
2025-08-16 18:14:36 +0700

    update globals.css and Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,43 @@
+"use client";
 import React from 'react'
 import Navbar from './Navbar'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Portfolio = () => {
     return (
-        <div style={{ color: "var(--fg)", backgroundColor: "var(--bg)", minHeight: "100vh" }}>
+        <div style={{ backgroundColor: "var(--bg)", minHeight: "100vh" }}>
             <Navbar />
             <div className='container py-5' id='home'>
                 <div className='row align-items-center'>
-                    {/* Kolom gambar */}
                     <div className='col-md-6 text-center'>
                         <img
                             src='./myself.png'
                             alt='Myself'
                             className='img-fluid'
-                            style={{ maxWidth: '500px', borderRadius: '100px' }}
+                            style={{ maxWidth: '500px', borderRadius: '10px' }}
                         />
                     </div>
-
-                    {/* Kolom teks */}
                     <div className='col-md-6 text-white'>
-                        <h1 className='fw-bold'>I'm Junior Natra Situmorang</h1>
+                        <h1 className='fw-bold'>
+                            <Typewriter
+                                words={["I'm Junior Natra Situmorang"]}
+                                loop={1}             // atau false, supaya cuma sekali
+                                cursor={false}
+                                typeSpeed={70}
+                                deleteSpeed={0}
+                            />
+                        </h1>
                         <p>
-                            Mahasiswa Teknik Informatika yang suka ngoprek teknologi, bikin projek, dan selalu penasaran hal baru.
+                            <Typewriter
+                                words={["I was interested at Software Engineering"]}
+                                loop={true}
+                                cursor
+                                cursorStyle="|"
+                                typeSpeed={70}
+                                deleteSpeed={100}
+                                delaySpeed={1500} />
                         </p>
                     </div>
-                </div>
-                <div>
-
                 </div>
             </div>
         </div>
