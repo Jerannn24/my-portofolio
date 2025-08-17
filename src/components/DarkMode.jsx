907acdc47889a35d0ba240184ff73746606165f9
Jerannn24 <juniornatra72@gmail.com>
2025-08-17 20:30:10 +0700

    update package.json and globals.css

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+
+const DarkMode = () => {
+    const [dark, setDark] = useState(false);
+    return (
+        <div>DarkMode</div>
+    )
+}
+
+export default DarkMode
