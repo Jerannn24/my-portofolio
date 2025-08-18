dcdbb18ba40c1fd0891b1801ff675ce0ee874762
Jerannn24 <juniornatra72@gmail.com>
2025-08-18 08:47:54 +0700

    update globals.css and ProjectList.js

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,14 +4,16 @@ import React from "react";
 // Contoh data project
 const projects = [
     {
-        title: "Project 1",
-        description: "Deskripsi project pertama.",
+        title: "Working ATM",
+        description: "Python ATM Basic Project",
         status: "Completed",
+        url: "https://github.com/Jerannn24/ATM"
     },
     {
         title: "Project 2",
         description: "Deskripsi project kedua.",
         status: "In Progress",
+        url: ""
     },
     {
         title: "Project 3",
@@ -33,26 +35,28 @@ const projects = [
 const ProjectList = () => {
     return (
         <div className="container py-4">
-            <div className="row g-4">
+            <div className="row g-4" >
                 {projects.map((project, index) => (
                     <div className="col-md-4" key={index}>
-                        <div className="card h-100 border-success shadow-sm">
-                            <div className="card-body" style={{ backgroundColor: 'var(--navbar-fg)' }}>
-                                <h5 className="card-title">{project.title}</h5>
-                                <p className="card-text">{project.description}</p>
-                            </div>
-                            <div className="card-footer text-end">
-                                <span
-                                    className={`badge ${project.status === "Completed"
-                                        ? "bg-success"
-                                        : project.status === "In Progress"
-                                            ? "bg-warning text-dark"
-                                            : "bg-primary"
-                                        }`}
-                                >
-                                    {project.status}
-                                </span>
-                            </div>
+                        <div className="card h-100 shadow-sm card-style" style={{ borderTopRightRadius: "10px", borderBottomLeftRadius: "10px" }}>
+                            <a href={project.url} target="_blank" className="text-decoration-none" style={{ color: "var(--fg)" }}>
+                                <div className="card-body card-style-body" style={{ backgroundColor: 'var(--navbar-bg)', borderTopRightRadius: "10px" }}>
+                                    <h5 className="card-title">{project.title}</h5>
+                                    <p className="card-text">{project.description}</p>
+                                </div>
+                                <div className="card-footer text-end" style={{ borderBottomLeftRadius: "10px" }}>
+                                    <span
+                                        className={`badge ${project.status === "Completed"
+                                            ? "bg-success"
+                                            : project.status === "In Progress"
+                                                ? "bg-warning text-dark"
+                                                : "bg-primary"
+                                            }`}
+                                    >
+                                        {project.status}
+                                    </span>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 ))}
